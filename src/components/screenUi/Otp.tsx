735a954ab6822484cb1893560cca4a800cae3b5d
Muhammad Suleman <msulaman061@gmail.com>
2024-07-10 15:16:49 +0500

    logic shifted to screen

--- src/components/screenUi/Otp.tsx
+++ src/components/screenUi/Otp.tsx
@@ -1,148 +1,110 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   TouchableOpacity,
-  Alert,
 } from 'react-native';
 
 import {AppColors} from '../../constants/AppColors';
 
-const Otp = () => {
-  const ot1 = useRef();
-  const ot2 = useRef();
-  const ot3 = useRef();
-  const ot4 = useRef();
+interface Iprops {
+  ot1: any;
+  ot2: any;
+  ot3: any;
+  ot4: any;
+  input1: string;
+  input2: string;
+  input3: string;
+  input4: string;
+  timer: number;
+  verifyOtpPressed: () => void;
+  onResendTimer: () => void;
+  onChangeText1: (val: any) => void;
+  onChangeText2: (val: any) => void;
+  onChangeText3: (val: any) => void;
+  onChangeText4: (val: any) => void;
+}
 
-  const [input1, setInput1] = useState('');
-  const [input2, setInput2] = useState('');
-  const [input3, setInput3] = useState('');
-  const [input4, setInput4] = useState('');
-
-  const [timer, setTimer] = useState(60);
-
-  useEffect(() => {
-    const timerInterval = setInterval(() => {
-      if (timer === 0) {
-        clearInterval(timerInterval);
-      } else {
-        setTimer(timer - 1);
-      }
-    }, 1000);
-
-    return () => {
-      clearInterval(timerInterval);
-    };
-  }, [timer]);
-
-  const verifyOtpPressed = () => {
-    let otp = '1234';
-    let enteredOtp = input1 + input2 + input3 + input4;
-    if (enteredOtp === otp) {
-      Alert.alert('OTP verified', 'You entered right OTP');
-    } else {
-      Alert.alert('Invalid input', 'You entered wrong OTP');
-    }
-  };
-
+const Otp = (props: Iprops) => {
   return (
     <View style={styles.container}>
       <View style={styles.otpView}>
         <TextInput
-          ref={ot1}
+          ref={props.ot1}
           maxLength={1}
-          value={input1}
+          value={props.input1}
           style={[
             styles.otpTextInput,
-            {borderColor: input1.length >= 1 ? 'green' : 'black'},
+            {borderColor: props.input1.length >= 1 ? 'green' : 'black'},
           ]}
           keyboardType="number-pad"
-          onChangeText={input => {
-            setInput1(input);
-            if (input.length >= 1) {
-              ot2.current.focus();
-            }
-          }}
+          onChangeText={(val: any) => props.onChangeText1(val)}
         />
         <TextInput
-          ref={ot2}
+          ref={props.ot2}
           maxLength={1}
-          value={input2}
+          value={props.input2}
           style={[
             styles.otpTextInput,
-            {borderColor: input2.length >= 1 ? 'green' : 'black'},
+            {borderColor: props.input2.length >= 1 ? 'green' : 'black'},
           ]}
           keyboardType="number-pad"
-          onChangeText={input => {
-            setInput2(input);
-            if (input.length >= 1) {
-              ot3.current.focus();
-            } else if (input.length < 1) {
-              ot1.current.focus();
-            }
-          }}
+          onChangeText={(val: any) => props.onChangeText2(val)}
         />
         <TextInput
-          ref={ot3}
+          ref={props.ot3}
           maxLength={1}
-          value={input3}
+          value={props.input3}
           style={[
             styles.otpTextInput,
-            {borderColor: input3.length >= 1 ? 'green' : 'black'},
+            {borderColor: props.input3.length >= 1 ? 'green' : 'black'},
           ]}
           keyboardType="number-pad"
-          onChangeText={input => {
-            setInput3(input);
-            if (input.length >= 1) {
-              ot4.current.focus();
-            } else if (input.length < 1) {
-              ot2.current.focus();
-            }
-          }}
+          onChangeText={(val: any) => props.onChangeText3(val)}
         />
         <TextInput
-          ref={ot4}
+          ref={props.ot4}
           maxLength={1}
-          value={input4}
+          value={props.input4}
           style={[
             styles.otpTextInput,
-            {borderColor: input4.length >= 1 ? 'green' : 'black'},
+            {borderColor: props.input4.length >= 1 ? 'green' : 'black'},
           ]}
           keyboardType="number-pad"
-          onChangeText={input => {
-            setInput4(input);
-            if (input.length < 1) {
-              ot3.current.focus();
-            }
-          }}
+          onChangeText={(val: any) => props.onChangeText4(val)}
         />
       </View>
 
       <View style={styles.timerView}>
         <TouchableOpacity
-          disabled={timer === 0 ? false : true}
-          onPress={() => setTimer(10)}>
+          disabled={props.timer === 0 ? false : true}
+          onPress={props.onResendTimer}>
           <Text
             style={[
               styles.resendText,
               {
-                color: timer == 0 ? 'blue' : 'gray',
+                color: props.timer == 0 ? 'blue' : 'gray',
               },
             ]}>
             Resend
           </Text>
         </TouchableOpacity>
 
-        {timer !== 0 && <Text style={styles.timerText}>{timer} seconds</Text>}
+        {props.timer !== 0 && (
+          <Text style={styles.timerText}>{props.timer} seconds</Text>
+        )}
       </View>
 
       <View>
         <TouchableOpacity
-          onPress={verifyOtpPressed}
+          onPress={props.verifyOtpPressed}
           disabled={
-            input1 !== '' && input2 !== '' && input3 !== '' && input4 !== ''
+            props.input1 !== '' &&
+            props.input2 !== '' &&
+            props.input3 !== '' &&
+            props.input4 !== ''
               ? false
               : true
           }
@@ -150,7 +112,10 @@ const Otp = () => {
             styles.verifyOtpButtonTouchable,
             {
               backgroundColor:
-                input1 !== '' && input2 !== '' && input3 !== '' && input4 !== ''
+                props.input1 !== '' &&
+                props.input2 !== '' &&
+                props.input3 !== '' &&
+                props.input4 !== ''
                   ? 'blue'
                   : 'gray',
             },
--- src/screens/OtpScreen.tsx
+++ src/screens/OtpScreen.tsx
@@ -1,10 +1,97 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {Alert} from 'react-native';
 
 import Otp from '../components/screenUi/Otp';
 
 const OtpScreen = () => {
-  return <Otp />;
+  const ot1 = useRef();
+  const ot2 = useRef();
+  const ot3 = useRef();
+  const ot4 = useRef();
+
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [input3, setInput3] = useState('');
+  const [input4, setInput4] = useState('');
+
+  const [timer, setTimer] = useState(60);
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      if (timer === 0) {
+        clearInterval(timerInterval);
+      } else {
+        setTimer(timer - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [timer]);
+
+  const verifyOtpPressed = () => {
+    let otp = '1234';
+    let enteredOtp = input1 + input2 + input3 + input4;
+    if (enteredOtp === otp) {
+      Alert.alert('OTP verified', 'You entered right OTP');
+    } else {
+      Alert.alert('Invalid input', 'You entered wrong OTP');
+    }
+  };
+
+  const onResendTimer = () => {
+    setTimer(60);
+  };
+
+  const onChangeText1 = (input: any) => {
+    setInput1(input);
+    if (input.length >= 1) {
+      ot2.current.focus();
+    }
+  };
+  const onChangeText2 = (input: any) => {
+    setInput2(input);
+    if (input.length >= 1) {
+      ot3.current.focus();
+    } else if (input.length < 1) {
+      ot1.current.focus();
+    }
+  };
+  const onChangeText3 = (input: any) => {
+    setInput3(input);
+    if (input.length >= 1) {
+      ot4.current.focus();
+    } else if (input.length < 1) {
+      ot2.current.focus();
+    }
+  };
+  const onChangeText4 = (input: any) => {
+    setInput4(input);
+    if (input.length < 1) {
+      ot3.current.focus();
+    }
+  };
+
+  return (
+    <Otp
+      ot1={ot1}
+      ot2={ot2}
+      ot3={ot3}
+      ot4={ot4}
+      input1={input1}
+      input2={input2}
+      input3={input3}
+      input4={input4}
+      timer={timer}
+      verifyOtpPressed={verifyOtpPressed}
+      onResendTimer={onResendTimer}
+      onChangeText1={onChangeText1}
+      onChangeText2={onChangeText2}
+      onChangeText3={onChangeText3}
+      onChangeText4={onChangeText4}
+    />
+  );
 };
 
 export default OtpScreen;
